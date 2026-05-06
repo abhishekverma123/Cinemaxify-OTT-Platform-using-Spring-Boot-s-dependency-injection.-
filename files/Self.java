fe37836fe4477dd56f57283ba22ac12dc427dbb1
ABHISHEK  VERMA <76154794+abhishekverma123@users.noreply.github.com>
2026-05-07 05:13:49 +0530

    Add files via upload
    . The User interface has the following methods:
    
        a. void setUserDetails(String name, int age, Long contact, String address): This method sets the user's name, age, contact, and address based on the provided parameters.
    
        b. void getUserDetails(): This method prints the user's details, including their name, member type, age, contact, and address.
    
    2. Complete the Self class by adding the below-mentioned attributes and methods:
    
          • name (String)
    
          • age (int)
    
          • contact (Long)
    
          • address (String)
    
          • Implement the User interface in the Self class and override the methods.
    
    3. Complete the Spouse class by adding the below-mentioned attributes and methods:
    
          • name (String)
    
          • age (int)
    
          • contact (Long)
    
          • address (String)
    
          • Implement the User interface in the Spouse class and override the methods.
    
    4. Create the beans for User implementations in the applicationContext.xml file as described in the template.
    
    5. In the main() method, load context from the applicationContext.xml file which is located in "src>main>resources".
    
    6. Create a console application that selects the user and saves details for the selected user as shown in the output below.
    
    7. Test your implementation by running the Main class.

---
+++ files/Self.java
@@ -0,0 +1,28 @@
+package com.example.Cinemaxify;
+
+// Override the User Interface methods in Self class
+public class Self implements User{
+
+    private String name;
+    private String memberName = "self";
+    private int age;
+    private Long contact;
+    private String address;
+	@Override
+	public void setUserDetails(String name, int age, Long contact, String address) {
+		// TODO Auto-generated method stub
+		
+		this.name = name;
+		this.age  = age;
+		this.contact = contact;
+		this.address = address;
+	}
+	@Override
+	public void getUserDetails() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello "+this.name+" you have entered the following details for "+this.memberName+"\nage : "+this.age+"\ncontact : "+this.contact+"\naddress: "+this.address);
+		
+	}
+    
+
+}
